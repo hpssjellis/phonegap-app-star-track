67687fa34de1f839de8ccbe7be5ae8b1f3832c2d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-04-23 07:09:21 -0700

    Create backup-www-zip.sh

---
+++ backup-www-zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash backup-www-zip.sh
+# best in cloud 9 to just right click this file and select run
+# change the /www below to any folder you wish to specifically backup
+
+
+
+zip -r my-www.zip ~/workspace/www
+
+
+echo "Right-click my-www.zip  --> select download to backup your workspace to windows"
